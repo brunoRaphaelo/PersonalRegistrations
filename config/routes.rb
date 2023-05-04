4f51e196ac2aff7b8efc7d4e02460c63d4a9a5d9
Bruno Ferreira <bruno@velow.com.br>
2023-05-03 23:37:35 -0300

    feat: created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  defaults format: :json do
+    resources :addresses, only: %i[index show create update destroy], controller: 'addresses'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    resources :persons, only: %i[index show create update destroy], controller: 'persons' do
+      resources :addesses, only: %i[index show], controller: 'persons/addresses'
+    end
+  end
 end
