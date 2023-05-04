ffe689073b08d3b9a2acc95a81c671eb2a09ce5d
Bruno Ferreira <bruno@velow.com.br>
2023-05-03 23:29:41 -0300

    feat: migration to create persons table

---
+++ db/migrate/20230504004403_create_addresses.rb
@@ -0,0 +1,18 @@
+class CreateAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :addresses do |t|
+      t.references :person, null: false
+
+      t.string :street, null: false
+      t.string :city, null: false
+      t.string :state, null: false
+      t.string :country, null: false
+
+      t.integer :postal_code, null: false
+
+      t.uuid :uuid, index: true, null: false
+
+      t.timestamps
+    end
+  end
+end
