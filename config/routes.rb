8fda91c657a1916f0c8e5532109670ad7da9c915
Bruno Ferreira <bruno@velow.com.br>
2023-05-04 16:02:19 -0300

    fix: adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   defaults format: :json do
     resources :addresses, only: %i[index show create update destroy], controller: 'addresses'
 
-    resources :persons, only: %i[index show create update destroy], controller: 'persons' do
-      resources :addesses, only: %i[index show], controller: 'persons/addresses'
-    end
+    resources :people, only: %i[index show create update destroy], controller: 'people'
   end
 end
