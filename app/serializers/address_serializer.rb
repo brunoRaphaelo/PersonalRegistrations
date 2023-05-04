ce832ef2d4679c939fe72f1c8dd8ae3874e9a004
Bruno Ferreira <bruno@velow.com.br>
2023-05-04 16:04:05 -0300

    feat: added serializers

---
+++ app/serializers/address_serializer.rb
@@ -0,0 +1,18 @@
+class AddressSerializer < ActiveModel::Serializer
+  attributes :id,
+             :person,
+             :street,
+             :state,
+             :city,
+             :postal_code,
+             :country
+
+  def person
+    return unless object.person_id
+
+    {
+      id: object.person_id,
+      name: object.person_name
+    }
+  end
+end
---
+++ app/serializers/person_serializer.rb
@@ -0,0 +1,11 @@
+class PersonSerializer < ActiveModel::Serializer
+  attributes :id,
+             :name,
+             :email,
+             :phone,
+             :birth_date
+
+  def birth_date
+    object.birth_date.strftime('%d-%m-%Y')
+  end
+end
