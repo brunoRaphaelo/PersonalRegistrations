451538628e0dbefbc09afa2c135994806ae4280e
Bruno Ferreira <bruno@velow.com.br>
2023-05-04 16:03:11 -0300

    feat: added classes validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,16 +1,41 @@
-class Person < ApplicationRecord
-  # Belongs to associations
-  belongs_to :person, inverse_of: :addresses, foreign_key: :person_id
+class Address < ApplicationRecord
+  # Belongs_to associations
+  belongs_to :person,
+             inverse_of: :addresses,
+             class_name: '::Person',
+             foreign_key: :person_id
 
   # Validations
   validates :street, :city, :state, :country, :postal_code, presence: true
-  validates :postal_code, numericality: { only_integer: true }
 
-  # Callbacks
-  before_validation :generate_uuid, on: :create
-
-  def generate_uuid
-    self.uuid = "p-#{::SecureRandom.uuid}"
-  end
+  # Scopes
+  scope :list, lambda {
+    select("#{table_name}.*")
+      .select("#{::Person.table_name}.name person_name")
+      .joins(:person)
+  }
+  scope :show, lambda {
+    select("#{table_name}.*")
+      .select("#{::Person.table_name}.name person_name")
+      .joins(:person)
+  }
+  scope :by_street, lambda { |street|
+    where("unaccent(#{table_name}.street) ILIKE :street",
+          street: "%#{I18n.transliterate(street.downcase)}%")
+  }
+  scope :by_city, lambda { |city|
+    where("unaccent(#{table_name}.city) ILIKE :city",
+          city: "%#{I18n.transliterate(city.downcase)}%")
+  }
+  scope :by_state, lambda { |state|
+    where("unaccent(#{table_name}.state) ILIKE :state",
+          state: "%#{I18n.transliterate(state.downcase)}%")
+  }
+  scope :by_country, lambda { |country|
+    where("unaccent(#{table_name}.country) ILIKE :country",
+          country: "%#{I18n.transliterate(country.downcase)}%")
+  }
+  scope :by_postal_code, ->(postal_code) { where(postal_code:) }
+  scope :by_person_id, ->(person_id) { where(person_id:) }
 end
 
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,17 +1,45 @@
 class Person < ApplicationRecord
   # Has many associations
-  has_many :addresses, inverse_of: :person, foreign_key: :person_id, dependent: :destroy
+  has_many :addresses,
+           inverse_of: :person,
+           class_name: '::Address',
+           foreign_key: :person_id,
+           dependent: :destroy
 
   # Validations
   validates :name, :email, :phone, :birth_date, presence: true
-  validates :status, numericality: { only_integer: true }
-  validates :discount_accepted, inclusion: { in: [true] }
+  validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }, if: :email_changed?
+  validates :phone, uniqueness: true, length: { minimum: 11, maximum: 13 }
+  validate :birth_date_range
 
-  # Callbacks
-  before_validation :generate_uuid, on: :create
+  # Scopes
+  scope :show, lambda {
+    select("#{table_name}.*")
+  }
+  scope :by_name, lambda { |name|
+    where("unaccent(#{table_name}.name) ILIKE :name",
+          name: "%#{I18n.transliterate(name.downcase)}%")
+  }
+  scope :by_email, lambda { |email|
+    where("unaccent(#{table_name}.email) ILIKE :email",
+          email: "%#{I18n.transliterate(email.downcase)}%")
+  }
+  scope :by_phone, lambda { |phone|
+    where("unaccent(#{table_name}.phone) ILIKE :phone",
+          phone: "%#{I18n.transliterate(phone.downcase)}%")
+  }
+  scope :by_birth_date, ->(birth_date) { where(birth_date:) }
 
-  def generate_uuid
-    self.uuid = "p-#{::SecureRandom.uuid}"
+  private
+
+  def birth_date_range
+    return unless birth_date
+
+    date_is_invalid = (birth_date < Date.today - 115.years) || birth_date > Date.today
+
+    return unless date_is_invalid
+
+    errors.add(:base, :invalid_date)
   end
 end
 
